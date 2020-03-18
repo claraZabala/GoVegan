031f2a7377300f622e39456376fd88746ef6743d
Clara <clzabala40@alumnes.ub.edu>
2020-03-18 14:48:13 +0100

    solucionat

---
+++ app/src/main/java/com/example/govegan/Calendari_Setmanal.kt
@@ -0,0 +1,12 @@
+package com.example.govegan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calendari_Setmanal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calendari_setmanal)
+    }
+}
