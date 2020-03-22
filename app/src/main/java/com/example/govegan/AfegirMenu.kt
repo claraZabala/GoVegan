20509941dec2bcd7ec056d78bbbdcccb4504af2e
Clara <clzabala40@alumnes.ub.edu>
2020-03-22 13:17:57 +0100

    intents varios

--- app/src/main/java/com/example/govegan/AfegirMenu.kt
+++ app/src/main/java/com/example/govegan/AfegirMenu.kt
@@ -1,11 +1,23 @@
 package com.example.govegan
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class AfegirMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.afegir_menu)
+    }
+
+    fun calendari(view: View) {
+        intent = Intent(this, Calendari_Setmanal::class.java)
+        startActivity(intent)
+    }
+
+    fun recepta(view: View){
+        intent = Intent(this, Recepta::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/govegan/Calendari_Setmanal.kt
+++ app/src/main/java/com/example/govegan/Calendari_Setmanal.kt
@@ -1,12 +1,19 @@
 package com.example.govegan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Calendari_Setmanal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendari_setmanal)
+    }
+
+    fun afegirMenu(view: View){
+        intent = Intent(this, AfegirMenu::class.java)
+        startActivity(intent)
     }
 }
