1d1a1a4d3255c77ca26e31a32170894bae44bd37
lluis <lrocarom@gmail.com>
2020-03-29 14:10:51 +0200

    Curiositats

--- app/src/main/java/com/example/govegan/model/Curiositats.kt
+++ app/src/main/java/com/example/govegan/model/Curiositats.kt
@@ -6,9 +6,29 @@ import java.lang.reflect.Constructor
 
 
 class Curiositats (imatge:Int, title:String, desc:String) {
-    var imatge:Int = 0;
-    var title:String = ""
-    var desc:String = ""
+    var imatge:Int
+        get(){
+            return imatge
+        }
+        set(newImatge){
+            this.imatge = newImatge
+        }
+    var title:String
+        get(){
+            return title
+        }
+        set(newTitle){
+            this.title = newTitle
+
+        }
+    var desc:String
+        get(){
+            return desc
+        }
+        set(newDesc){
+            this.desc = newDesc
+
+        }
     init{
         this.imatge = imatge
         this.title = title
