03dc1de1805ad5e8a0805064bf7fb60e3b9fc904
angelass000 <angseran@gmail.com>
2020-03-29 22:10:14 +0200

    classe recepta en continuació

--- app/src/main/java/com/example/govegan/vista/Recepta.kt
+++ app/src/main/java/com/example/govegan/vista/Recepta.kt
@@ -5,8 +5,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.govegan.R
+import com.example.govegan.model.Ingredient
 
 class Recepta : AppCompatActivity() {
+
+    var nom:String = ""
+    var tempsPreparacio:Float = 0.0F
+    var numPersones : Int = 0
+    var descripcio : String = ""
+    val ingredients = arrayOf<Ingredient>()
+    var imatge : String = ""
+    var llistaIcones = arrayOf("camiIcona1(ex:ous)", "camiIcona2(ex:carn)", "camiIcona3(ex:caraHappy)", "camiIcona4(ex:gluten)")
+    var esCertificat = false
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
