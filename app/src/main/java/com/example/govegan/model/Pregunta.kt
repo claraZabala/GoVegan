a7439bf805bada8dd99becd5224b3c982e827f59
angelass000 <angseran@gmail.com>
2020-04-01 16:42:52 +0200

    forum en procés

--- app/src/main/java/com/example/govegan/model/Pregunta.kt
+++ app/src/main/java/com/example/govegan/model/Pregunta.kt
@@ -11,4 +11,15 @@ open class Pregunta (imatge:Int, title:String, desc:String) {
     var descripcio:String = ""
     val respostes = arrayOf<Resposta>()
     val tema: String = ""
+
+
+    //Creem un diccionari de clau-valor fins a que puguem treballar amb la base de dades
+    // La clau serà la descripció de la pregunta i el valor l'id de la pregunta
+    //En aquest trobarem 3 exemples de preguntes
+    var myMap: Map<String, Int> = mutableMapOf("On puc comprar Heura?" to 1, "La marca Sephora és vegana?" to 2, "Quins plats són recomanables per aconseguir massa muscular?" to 3)
+
+    fun afegirPregunta(){
+        
+    }
+
 }
