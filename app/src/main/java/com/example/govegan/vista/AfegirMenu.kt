e0df90b24f208e76a5ab4b204be2ffca59ac230c
lluis <lrocarom@gmail.com>
2020-04-01 21:06:57 +0200

    dialogIngredients

--- app/src/main/java/com/example/govegan/vista/AfegirMenu.kt
+++ app/src/main/java/com/example/govegan/vista/AfegirMenu.kt
@@ -3,6 +3,7 @@ package com.example.govegan.vista
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.govegan.R
 
@@ -10,6 +11,12 @@ class AfegirMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.afegir_menu)
+        val dialog =  AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_ingredients,null)
+        dialog.setView(dialogView)
+        dialog.setCancelable(false)
+        dialog.show()
+
     }
 
     fun calendari(view: View) {
