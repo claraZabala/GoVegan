9c9f7a575223a498cabda926f63a9f49d02957b7
Clara <clzabala40@alumnes.ub.edu>
2020-04-01 18:53:55 +0200

    Canvi nom més intentar afegir el meu commit a la branca d'abans

--- app/src/main/java/com/example/govegan/controlador/Controlador.kt
+++ app/src/main/java/com/example/govegan/controlador/Controlador.kt
@@ -1,57 +1,59 @@
 package com.example.govegan.controlador
 
 import com.example.govegan.model.CarteraUsuaris
-import com.example.govegan.model.Usuari
-
 import com.example.govegan.model.Curiositat
 
 class Controlador {
-    var façanaCarteraCuriositats:FaçanaCarteraCuriositats
-    var carteraUsuaris:CarteraUsuaris
-    init{
-        façanaCarteraCuriositats = FaçanaCarteraCuriositats()
+    var facadeCarteraCuriositats: FacadeCarteraCuriositats
+    var carteraUsuaris: CarteraUsuaris
+
+    init {
+        facadeCarteraCuriositats = FacadeCarteraCuriositats()
         carteraUsuaris = CarteraUsuaris()
     }
 
-    fun registre(nom: String, cognoms: String, nomUsuari: String, mail: String, pwd: String,
-                 pwd2: String, edat: String): Int {
+    fun registre(
+        nom: String, cognoms: String, nomUsuari: String, mail: String, pwd: String,
+        pwd2: String, edat: String
+    ): Int {
         if (pwd.isNullOrBlank() or pwd2.isNullOrBlank() or mail.isNullOrBlank() or nom.isNullOrBlank()
-        or edat.isNullOrBlank() or cognoms.isNullOrBlank() or nomUsuari.isNullOrBlank()) {
+            or edat.isNullOrBlank() or cognoms.isNullOrBlank() or nomUsuari.isNullOrBlank()
+        ) {
             return 1
-        }
-        else if (!pwd.equals(pwd2)){
+        } else if (!pwd.equals(pwd2)) {
             return 2
-        }
-        else {
-            if (carteraUsuaris.registre(nom, cognoms, nomUsuari, mail, pwd, edat)){
+        } else {
+            if (carteraUsuaris.registre(nom, cognoms, nomUsuari, mail, pwd, edat)) {
                 return 0
-            }
-            else {
+            } else {
                 return 3
             }
         }
     }
 
-    fun getLlistaCuriositats():ArrayList<Curiositat> {
-    return façanaCarteraCuriositats.getLlistaCuriositats()
-
+    fun getLlistaCuriositats(): ArrayList<Curiositat> {
+        return facadeCarteraCuriositats.getLlistaCuriositats()
     }
 
     fun getCuriositatByTheme(tema: String): Curiositat? {
-        return façanaCarteraCuriositats.getCuriositatByTheme(tema)
-    }
-    fun changeDescCuriositat(tema: String,descNova: String):Boolean{
-        return façanaCarteraCuriositats.changeDescCuriositat(tema,descNova)
-    }
-    fun addCuriositat(tema:String,desc:String,imatge:Int):Boolean{
-        return façanaCarteraCuriositats.addCuriositat(tema,desc,imatge)
+        return facadeCarteraCuriositats.getCuriositatByTheme(tema)
     }
 
-    fun removeCuriositat(index: Int){
-        return façanaCarteraCuriositats.removeCuriositat(index)
+    fun changeDescCuriositat(tema: String, descNova: String): Boolean {
+        return facadeCarteraCuriositats.changeDescCuriositat(tema, descNova)
+    }
+
+    fun addCuriositat(tema: String, desc: String, imatge: Int): Boolean {
+        return facadeCarteraCuriositats.addCuriositat(tema, desc, imatge)
+    }
+
+    fun removeCuriositat(index: Int) {
+        return facadeCarteraCuriositats.removeCuriositat(index)
     }
 
 
-    fun getIndexFromList(tema:String):Int{
-        return façanaCarteraCuriositats.getIndexFromList(tema)
+    fun getIndexFromList(tema: String): Int {
+        return facadeCarteraCuriositats.getIndexFromList(tema)
+    }
+
 }
