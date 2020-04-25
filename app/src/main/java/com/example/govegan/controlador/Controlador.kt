ff7b3e204aba8fca5346074d23a1f13db11ebc08
Clara <clzabala40@alumnes.ub.edu>
2020-04-25 19:38:38 +0200

    Controlador ordenat

--- app/src/main/java/com/example/govegan/controlador/Controlador.kt
+++ app/src/main/java/com/example/govegan/controlador/Controlador.kt
@@ -19,6 +19,11 @@ object Controlador {
     val baseDades: BaseDades
     val db: FirebaseFirestore
 
+    /**
+     * INITS *********************
+     *      **********************
+     */
+
     init {
         db = FirebaseFirestore.getInstance()
         baseDades = BaseDades(db)
@@ -37,6 +42,11 @@ object Controlador {
         Toast.makeText(this, missatge, Toast.LENGTH_LONG).show()
     }
 
+    /**
+     * USER *********************
+     *      **********************
+     */
+
     fun getUsuariActiu(): Usuari? {
         return usuariActiu
     }
@@ -54,73 +64,17 @@ object Controlador {
         return facadeCarteraUsuaris.login(nomUsuari, pwd)
     }
 
+    /**
+     * RECEPTA *********************
+     *         **********************
+     */
+
     fun setReceptaActiva(recepta: Proposta?){
         receptaActiva = recepta
     }
 
     fun getReceptaActiva(): Proposta? {
         return receptaActiva
-    }
-
-    fun getLlistaCuriositats(): ArrayList<Curiositat> {
-        return facadeCarteraCuriositats.getLlistaCuriositats()
-    }
-
-    fun getCuriositatByTheme(tema: String): Curiositat? {
-        return facadeCarteraCuriositats.getCuriositatByTheme(tema)
-    }
-
-    fun changeDescCuriositat(tema: String, descNova: String): Boolean {
-        return facadeCarteraCuriositats.changeDescCuriositat(tema, descNova)
-    }
-
-    fun addCuriositat(tema: String, desc: String, imatge: Int): Boolean {
-        return facadeCarteraCuriositats.addCuriositat(tema, desc, imatge)
-    }
-
-    fun removeCuriositat(index: Int) {
-        return facadeCarteraCuriositats.removeCuriositat(index)
-    }
-
-
-    fun getIndexFromList(tema: String): Int {
-        return facadeCarteraCuriositats.getIndexFromList(tema)
-    }
-
-    fun getIngredientsByName(nomIngredient: String): Ingredient?{
-        return facadeCarteraIngredients.getIngredientsByName(nomIngredient)
-    }
-
-    fun getNameIngredients():ArrayList<String>{
-        return facadeCarteraIngredients.getNameIngredients()
-    }
-
-    fun addNouIngredientAmbFoto(nomIngredient: String,fotoInt: Int){
-        facadeCarteraIngredients.addNouIngredientAmbFoto(nomIngredient,fotoInt)
-    }
-
-    fun addNouIngredientSenseFoto(nomIngredient: String){
-        facadeCarteraIngredients.addNouIngredientSenseFoto(nomIngredient)
-    }
-
-    fun getAllIngredientsByName():ArrayList<String>{
-        return facadeCarteraIngredients.getAllIngredientsByName()
-    }
-
-    fun crearPregunta(idUsuari: String, descripcio: String, tema: String){
-        facadeCarteraPreguntes.crearPreguntaF(idUsuari, descripcio, tema)
-    }
-
-    fun mostrarPreguntesPerTema(temaP: String): ArrayList<Pregunta>?{
-        return facadeCarteraPreguntes.mostrarPreguntesPerTemaF(temaP)
-    }
-
-    fun crearResposta(tema: String, descripcio: String, esCertificat: Boolean, idUsuari: String, idDestinatari: String){
-        facadeCarteraPreguntes.crearRespostaF(tema, descripcio, esCertificat, idUsuari, idDestinatari)
-    }
-
-    fun mostrarRespPerIdPreg(tema:  String, descripcio: String, esCertificat: Boolean, idUsuari: String, idDestinatari: String): ArrayList<Resposta>?{
-        return facadeCarteraPreguntes.mostrarRespPerIdPregF(tema, descripcio, esCertificat, idUsuari, idDestinatari)
     }
 
     fun setReceptaFromProposta(titolRecepta: String) {
@@ -153,6 +107,65 @@ object Controlador {
         return 2
     }
 
+    fun getReceptaByName(nom: String) {
+        var recepta = facadeCarteraReceptes.getReceptaByName(nom)
+        setReceptaActiva(recepta)
+    }
+
+    /**
+     * CURIOSITATS *********************
+     *             **********************
+     */
+
+    fun getLlistaCuriositats(): ArrayList<Curiositat> {
+        return facadeCarteraCuriositats.getLlistaCuriositats()
+    }
+
+    fun getCuriositatByTheme(tema: String): Curiositat? {
+        return facadeCarteraCuriositats.getCuriositatByTheme(tema)
+    }
+
+    fun changeDescCuriositat(tema: String, descNova: String): Boolean {
+        return facadeCarteraCuriositats.changeDescCuriositat(tema, descNova)
+    }
+
+    fun addCuriositat(tema: String, desc: String, imatge: Int): Boolean {
+        return facadeCarteraCuriositats.addCuriositat(tema, desc, imatge)
+    }
+
+    fun removeCuriositat(index: Int) {
+        return facadeCarteraCuriositats.removeCuriositat(index)
+    }
+
+    fun getIndexFromList(tema: String): Int {
+        return facadeCarteraCuriositats.getIndexFromList(tema)
+    }
+
+    /**
+     * INGREDIENTS *********************
+     *             **********************
+     */
+
+    fun getIngredientsByName(nomIngredient: String): Ingredient?{
+        return facadeCarteraIngredients.getIngredientsByName(nomIngredient)
+    }
+
+    fun getNameIngredients():ArrayList<String>{
+        return facadeCarteraIngredients.getNameIngredients()
+    }
+
+    fun addNouIngredientAmbFoto(nomIngredient: String,fotoInt: Int){
+        facadeCarteraIngredients.addNouIngredientAmbFoto(nomIngredient,fotoInt)
+    }
+
+    fun addNouIngredientSenseFoto(nomIngredient: String){
+        facadeCarteraIngredients.addNouIngredientSenseFoto(nomIngredient)
+    }
+
+    fun getAllIngredientsByName():ArrayList<String>{
+        return facadeCarteraIngredients.getAllIngredientsByName()
+    }
+
     fun afegirIngredientLlistaCompra(ingredient: String): Boolean {
         return facadeCarteraUsuaris.afegirIngredientLlistaCompra(getUsuariActiu()?.nomUsuari,ingredient)
     }
@@ -170,8 +183,24 @@ object Controlador {
         return facadeCarteraUsuaris.getLlistaUsuari(getUsuariActiu()?.nomUsuari)
     }
 
-    fun getReceptaByName(nom: String) {
-        var recepta = facadeCarteraReceptes.getReceptaByName(nom)
-        setReceptaActiva(recepta)
+    /**
+     * FORUM *********************
+     *      **********************
+     */
+
+    fun crearPregunta(idUsuari: String, descripcio: String, tema: String){
+        facadeCarteraPreguntes.crearPreguntaF(idUsuari, descripcio, tema)
+    }
+
+    fun mostrarPreguntesPerTema(temaP: String): ArrayList<Pregunta>?{
+        return facadeCarteraPreguntes.mostrarPreguntesPerTemaF(temaP)
+    }
+
+    fun crearResposta(tema: String, descripcio: String, esCertificat: Boolean, idUsuari: String, idDestinatari: String){
+        facadeCarteraPreguntes.crearRespostaF(tema, descripcio, esCertificat, idUsuari, idDestinatari)
+    }
+
+    fun mostrarRespPerIdPreg(tema:  String, descripcio: String, esCertificat: Boolean, idUsuari: String, idDestinatari: String): ArrayList<Resposta>?{
+        return facadeCarteraPreguntes.mostrarRespPerIdPregF(tema, descripcio, esCertificat, idUsuari, idDestinatari)
     }
 }
