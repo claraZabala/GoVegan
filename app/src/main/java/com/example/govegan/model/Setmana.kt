127857ac3ddf9086dc36cd04aa8c9395df20f7b0
Clara <clzabala40@alumnes.ub.edu>
2020-04-26 18:23:11 +0200

    dia i setmana

--- app/src/main/java/com/example/govegan/model/Setmana.kt
+++ app/src/main/java/com/example/govegan/model/Setmana.kt
@@ -2,18 +2,20 @@ package com.example.govegan.model
 
 class Setmana (usuari: Usuari, id: Int){
 
-
     var id: Int = 0
     var usuari: Usuari
     var dies: ArrayList<Dia> = ArrayList(7)
-
-
 
     init {
         this.id = id
         this.usuari = usuari
         dies.add(Dia(id, "dilluns"))
-        //TODO:afegir altres dies
+        dies.add(Dia(id,"dimarts"))
+        dies.add(Dia(id,"dimecres"))
+        dies.add(Dia(id,"dijous"))
+        dies.add(Dia(id,"divendres"))
+        dies.add(Dia(id,"dissabte"))
+        dies.add(Dia(id,"diumenge"))
     }
 
     fun getName(): String {
